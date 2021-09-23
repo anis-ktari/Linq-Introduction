89a9571ce2d27a23f0f5514f8237e7f928251c70
anis-ktari <private>
2021-09-23 12:28:18 +0200

    Add Rrestriction examples

---
+++ Linq-Introduction-Test/RestrictionsTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Linq_Introduction_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Should_return_low_numbers()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
